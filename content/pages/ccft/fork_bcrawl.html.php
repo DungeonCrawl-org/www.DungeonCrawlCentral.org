71d586c50caa25b59a1b8a906a05298d1b8864fb
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-19 06:53:06 -0400

    Update fork_bcrawl.html.php

--- content/pages/ccft/fork_bcrawl.html.php
+++ content/pages/ccft/fork_bcrawl.html.php
@@ -4,3 +4,10 @@
 
 <p>The <a href="https://github.com/b-crawl/bcrawl" target="_blank"><b>BCrawl Fork</b></a> is full of changes even though it retains the food consumption aspect.</p>
 <p>For a text friendly writeup, see the CHANGELOG on <a href="https://github.com/b-crawl/bcrawl/blob/master/CHANGES.md" target="_blank">github.com/b-crawl/bcrawl/blob/master/CHANGES.md</a></p>
+
+<h4>BCrawl servers:</h4>
+<ul><li>CNC Korea:     https://crawl.nemelex.cards/ </li>
+    <li>CCO Toronto:   http://cco.crawlcosplay.org/ </li>
+    <li>CYO Seattle:   https://crawl.yiuf.org/ </li>
+    <li>CUE Germany:   https://underhound.eu:8080/</li>
+</ul>
