39fad16ef0dca11ad6dc26c40479ab3e99fd0912
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-19 06:57:54 -0400

    Update fork_bcrawl.html.php

--- content/pages/ccft/fork_bcrawl.html.php
+++ content/pages/ccft/fork_bcrawl.html.php
@@ -6,8 +6,8 @@
 <p>For a text friendly writeup, see the CHANGELOG on <a href="https://github.com/b-crawl/bcrawl/blob/master/CHANGES.md" target="_blank">github.com/b-crawl/bcrawl/blob/master/CHANGES.md</a></p>
 
 <h4>BCrawl servers:</h4>
-<ul><li>CNC Korea:     https://crawl.nemelex.cards/ </li>
-    <li>CCO Toronto:   http://cco.crawlcosplay.org/ </li>
-    <li>CYO Seattle:   https://crawl.yiuf.org/ </li>
-    <li>CUE Germany:   https://underhound.eu:8080/</li>
+<ul><li><a href="https://crawl.nemelex.cards/" target="_blank">CNC in South Korea</a></li>
+    <li><a href="http://cco.crawlcosplay.org/" target="_blank">CCO Toronto</a></li>
+    <li><a href="https://crawl.yiuf.org/" target="_blank">CYO in Seattle</li>
+    <li><a href="https://underhound.eu:8080//" target="_blank">CUE in Germany</li>
 </ul>
