8fe9c5b8f992004edb3c3a917e847508244d3b0c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-20 02:23:58 -0400

    Update fchallengedetails.html.php

--- content/pages/ccft/fchallengedetails.html.php
+++ content/pages/ccft/fchallengedetails.html.php
@@ -40,7 +40,7 @@ $this->setData("meta", ['filename' => $cha->icon]);
 	<?php endif;?>
 	<?php if ($cha->wiki): ?> |
 	<a href="<?=$e($cha->wiki)?>" target="_blank">Wiki page</a><?php endif; ?>
-	| <a href="<?=$e($cha->reddit)?>" target="_blank">Fork's details</a>
+	| <a href="<?=$e($cha->reddit)?>">Fork's details</a>
 </p>
 
 <?php if ($cha->icon) : ?><img src="<?=$e($cha->icon)?>" class="detail" /><?php endif; ?>
