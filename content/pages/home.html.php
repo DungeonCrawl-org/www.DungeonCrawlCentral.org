cc4319403ad23a7b5967afb7a2308a26b49561fc
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-21 15:26:53 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -8,7 +8,7 @@
 	echo "<h2>" . _("Welcome to the Crawl Cosplay 5-in-1 website!") . "</h2>";
 	echo '<img src="'.$dir_path."/".$filename.'" alt="'.$filename.'" style="float:right">';
 
-	echo '<h3><a href="https://crawl.develz.org/" target="_blank"> Dungeon Crawl: Stone Soup </a> (DCSS)' . _("is the game!") . "</h3>";
+	echo '<h3><a href="https://crawl.develz.org/" target="_blank"> Dungeon Crawl: Stone Soup</a> (DCSS) ' . _("is the game!") . "</h3>";
 	echo '<ul><li><a href="https://crawl.develz.org/play.htm" target="_blank">' . _("Play online on a Webtiles server") . "</a>.</li>";
 	echo     '<li><a href="https://crawl.develz.org/download.htm" target="_blank">' . _("Download the offline version") . "</a>.</li></ul>";
 
