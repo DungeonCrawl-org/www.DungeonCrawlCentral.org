d13bcb96ff6f58d08989ddbcf0226ae2b0e72c66
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-21 09:22:32 -0400

    Update fork_bcrawl.html.php

--- content/pages/ccft/fork_bcrawl.html.php
+++ content/pages/ccft/fork_bcrawl.html.php
@@ -3,7 +3,8 @@
 <h2>About fork BCrawl</h2>
 
 <p>The <a href="https://github.com/b-crawl/bcrawl" target="_blank"><b>BCrawl Fork</b></a> is full of changes even though it retains the food consumption aspect.</p>
-<p>For a text friendly writeup, see the CHANGELOG on <a href="https://github.com/b-crawl/bcrawl/blob/master/CHANGES.md" target="_blank">github.com/b-crawl/bcrawl/blob/master/CHANGES.md</a></p>
+<p>For a text friendly writeup, see the CHANGELOG on 
+   <a href="https://github.com/b-crawl/bcrawl/blob/master/CHANGES.md" target="_blank">github.com/b-crawl/bcrawl/blob/master/CHANGES.md</a></p>
 
 <h4>BCrawl servers:</h4>
 <ul><li><a href="https://crawl.nemelex.cards/" target="_blank">CNC in South Korea</a></li>
@@ -11,3 +12,5 @@
     <li><a href="https://crawl.yiuf.org/" target="_blank">CYO in Seattle</li>
     <li><a href="https://underhound.eu:8080//" target="_blank">CUE in Germany</li>
 </ul>
+
+<p>Join the <a href="https://discord.gg/PR2na4pATB" target="_blank">bcrawl discord server</a></p>
