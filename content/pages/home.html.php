86e1e273463a77d9f6cc779083dacebccf547381
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-21 08:44:58 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -33,14 +33,15 @@
 	echo     "<li>" . _("Next CCSDT for v0.33 in mid 2025.") . "</li>";
 	echo     "<li>" . _("Read") . ': <a href="/ccsdt/about_ccsdt">' . _("About CCSDT") . "</a>.</li></ul>";
 
-	echo '<h3><a href="/ccft">' . _("Crawl Cosplay Fork Tournament") . "</a> (CCFT) " . _("NEW!") . "</h3>";
+	echo '<h3><a href="/ccft">' . _("Crawl Cosplay Fork Tournament") . "</a> (CCFT) " . _("NEW & Upcoming!") . "</h3>";
 	echo "<ul><li>" . _("A tournament lasting about a 4-5 weeks with each week highlighting a different DCSS Fork.") . "</li>";
 	echo     "<li>" . _("The first ever CCFT will likely start on May 30th, 2025.");
 	echo     "<li>" . _("Read a draft of") . ' <a href="/ccft/about_ccft">' . _("About CCFT") . "</a>.</li></ul>";
-?>
 	
-<h2>Watch a video about the Crawl Cosplay website</h2>
-<p>For indepth details about the Crawl Cosplay website, see: <a href="https://www.youtube.com/watch?v=x7u0PwESwPM&t=39s" target="_blank">Dumalacath's CC video</a></p>
+	echo "<h2>" . _("Watch a video about the Crawl Cosplay website") . "</h2>";
+	echo "<p>" . _("For indepth details about the Crawl Cosplay website, see:")
+		   . '<a href="https://www.youtube.com/watch?v=x7u0PwESwPM&t=39s" target="_blank">' . _("Dumalacath's CC video") . "</a></p>";
+?>
 
 <h2>Want to chat?</h2>
 <p>Come chat with us on our 
