426dcb93554729c965a579595f6442754017d1d7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-21 09:38:35 -0400

    Update fork_stoatsoup.html.php

--- content/pages/ccft/fork_stoatsoup.html.php
+++ content/pages/ccft/fork_stoatsoup.html.php
@@ -2,4 +2,9 @@
 
 <h2>About fork Stoat Soup</h2>
 
-<p>The <b>Stoat Soup fork</b> is...</p>
+<h4>Webtiles servers:</h4>
+<ul><li><a href="https://crawl.nemelex.cards/" target="_blank">CNC in South Korea</a></li>
+    <li><a href="http://cco.crawlcosplay.org/" target="_blank">CCO Toronto</a></li>
+    <li><a href="https://crawl.yiuf.org/" target="_blank">CYO in Seattle</a></li>
+    <li><a href="https://crawl.project357.org/" target="_blank">CPO in Australia</a></li>
+</ul>
