f233f95ffc4a1948bcbb7863a5041af06ee40a27
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-22 18:02:52 -0400

    Delete content/pages/dcss.org/home.html.php

--- content/pages/dcss.org/home.html.php
+++
@@ -1,3 +0,0 @@
-<?php $this->layout = 'dcss.org'; ?>
-
-<h2>An open source roguelike adventure through dungeons filled with dangerous monsters in a quest to find the mystifyingly fabulous Orb of Zot.</h2>
