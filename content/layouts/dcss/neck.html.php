61bc1ba7b32a91adb15833e4882060f78c6219a7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-22 18:05:13 -0400

    Create neck.html.php

---
+++ content/layouts/dcss/neck.html.php
@@ -0,0 +1,26 @@
+<div class="page information">
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">Menu</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/dcss.org/home">Dungeon Crawl: Stone Soup</a>
+			<a href="/dcss.org/online"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online</a>
+			<a href="/dcss.org/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download DCSS</a>
+			<a href="http://crawl.chaosforge.org/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; DCSS Wiki</a>
+		</div>
+	</div>
+
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Donate!</button>
+		<div class="dropdown-content" style="right:0;">
+			Please donate for the cost of the CCO.CrawlCosplay.Org webtiles server at:<br>
+			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">RoGGa's Patreon</b></a>
+		</div>
+	</div>    <div class="toplinks"><br></div>
+    <div class="page_content">
+        <div class="heading">
+            <h1><img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /> Dungeon Crawl: Stone Soup <img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
+	          <div class="fineprint"><center>Come chat with us in our <a href="https://discord.gg/pW7nqC8Wu3" target="_blank">DCSS Crawl Cosplay 2.0 community discord server
+                                           <img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
+        </div>
+	<br>
+        <img src="/img/HR-right.png"><br>
