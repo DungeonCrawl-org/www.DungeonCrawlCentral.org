970154b119d5d140b1ae5daba828964e5e3c874e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-23 02:34:28 -0400

    Update online.html.php

--- content/pages/dcss/online.html.php
+++ content/pages/dcss/online.html.php
@@ -1,10 +1,14 @@
-Select a server manually:
+<?php $this->layout = 'dcss'; ?>
 
-Sydney, Australia: crawl.project357.org
-Arizona, USA: crawl.akrasiac.org
-Ohio, USA: cbro.berotato.org
-Paris, France: crawl.xtahua.com
-Falkenstein, Germany: underhound.eu
-Gyeonggi, Korea: crawl.nemelex.cards
-Tokyo, Japan: lazy-life.ddo.jp
-New York, USA: crawl.dcss.io
+<h4>Select a server manually:</h4>
+
+<ul><li>Toronto, Canada: cco.crawlcosplay.org</li>
+    <li>Arizona, USA: crawl.akrasiac.org</li>
+    <li>Ohio, USA: cbro.berotato.org</li>
+    <li>New York, USA: crawl.dcss.io</li>
+    <li>Paris, France: crawl.xtahua.com</li>
+    <li>Falkenstein, Germany: underhound.eu</li>
+    <li>Gyeonggi, Korea: crawl.nemelex.cards</li>
+    <li>Tokyo, Japan: lazy-life.ddo.jp</li>
+    <li>Sydney, Australia: crawl.project357.org</li>
+</ul>
