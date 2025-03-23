a481b820f96402ec2b45e708091b8a9e8a32a65c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-23 02:07:36 -0400

    Create online.html.php

---
+++ content/pages/dcss/online.html.php
@@ -0,0 +1,10 @@
+Select a server manually:
+
+Sydney, Australia: crawl.project357.org
+Arizona, USA: crawl.akrasiac.org
+Ohio, USA: cbro.berotato.org
+Paris, France: crawl.xtahua.com
+Falkenstein, Germany: underhound.eu
+Gyeonggi, Korea: crawl.nemelex.cards
+Tokyo, Japan: lazy-life.ddo.jp
+New York, USA: crawl.dcss.io
