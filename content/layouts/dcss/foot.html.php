15e841e82ea47fc109715f4da477807913736692
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-23 01:40:14 -0400

    Update foot.html.php

--- content/layouts/dcss/foot.html.php
+++ content/layouts/dcss/foot.html.php
@@ -2,6 +2,7 @@
 		<img src="/img/HR-right.png"><br />
 		<br />
 		<footer>Copyleft  |  <a href="https://github.com/DCSS-CrawlCosplay-org/crawl_cosplay_org/issues" target="_blank">Send bug report or feature request</a></footer>
+		<footer style="text-align:right;">You are too berserk! Calm down, please.</footer>
 		</div>
 	</div>
 </div>
