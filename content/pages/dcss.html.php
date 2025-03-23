dbc48795c42e971eb65b64f9c1c4d23d25f9a8e7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-23 01:58:01 -0400

    Update dcss.html.php

--- content/pages/dcss.html.php
+++ content/pages/dcss.html.php
@@ -3,3 +3,5 @@
 <h2 style="color:green"><img src="/img/feat-dg_entrance.png"> A free & open source roguelike adventure through dungeons filled with dangerous monsters in a quest to find the fabulous:</h2>
 <h1>Orb of Zot</h1>
 <center><img src="/img/orb_of_zot.gif"></center>
+
+<h1>Play Online - Download DCSS</h1>
