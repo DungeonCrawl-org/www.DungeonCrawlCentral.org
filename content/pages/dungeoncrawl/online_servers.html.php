551e2b992b48737eeaf370f7be18262d48165f92
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 19:05:50 -0400

    Create online_servers.html.php

---
+++ content/pages/dungeoncrawl/online_servers.html.php
@@ -0,0 +1,14 @@
+<?php $this->layout = 'dungeoncrawl'; ?>
+
+<h2>Select a server manually:</h2>
+
+<ul><li>Toronto, Canada: cco.crawlcosplay.org</li>
+    <li>Arizona, USA: crawl.akrasiac.org</li>
+    <li>Ohio, USA: cbro.berotato.org</li>
+    <li>New York, USA: crawl.dcss.io</li>
+    <li>Paris, France: crawl.xtahua.com</li>
+    <li>Falkenstein, Germany: underhound.eu</li>
+    <li>Gyeonggi, Korea: crawl.nemelex.cards</li>
+    <li>Tokyo, Japan: lazy-life.ddo.jp</li>
+    <li>Sydney, Australia: crawl.project357.org</li>
+</ul>
