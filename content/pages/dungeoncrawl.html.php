b68da6cc86e2dba99609251858a7b30c07223b9a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 19:52:38 -0400

    Update dungeoncrawl.html.php

--- content/pages/dungeoncrawl.html.php
+++ content/pages/dungeoncrawl.html.php
@@ -1,6 +1,7 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h2 style="color:green"><img src="/img/feat-dg_entrance.png"> A free & open source roguelike adventure through dungeons filled with dangerous monsters in a quest to find the fabulous:</h2>
+<h2 style="color:green">Your home for free, open source & actively developped Dungeon Crawl forks.</h2>
+<h2 style="color:green"><img src="/img/feat-dg_entrance.png">Adventure through dungeons filled with dangerous monsters in a quest to find the fabulous:</h2>
 <h1>Orb of Zot</h1>
 <center><img src="/img/orb_of_zot.gif"></center>
 
