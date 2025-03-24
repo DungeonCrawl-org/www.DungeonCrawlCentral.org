609ed95de806689391c3ca9eaa5f5bd969c8dca2
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 16:20:59 -0400

    Create dungeoncrawl.html.php

---
+++ content/pages/dungeoncrawl.html.php
@@ -0,0 +1,7 @@
+<?php $this->layout = 'dungeoncrawl'; ?>
+
+<h2 style="color:green"><img src="/img/feat-dg_entrance.png"> A free & open source roguelike adventure through dungeons filled with dangerous monsters in a quest to find the fabulous:</h2>
+<h1>Orb of Zot</h1>
+<center><img src="/img/orb_of_zot.gif"></center>
+
+<h2><center><a href="/dcss/online">Play Online</a> - <a href="dcss/download">Download DCSS</a></center></h2>
