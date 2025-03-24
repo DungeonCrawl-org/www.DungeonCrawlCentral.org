2132575b719b6e4f3d2dba9f21035370bc4a4a25
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 16:28:40 -0400

    Create neck.html.php

---
+++ content/layouts/dungeoncrawl/neck.html.php
@@ -0,0 +1,25 @@
+<div class="page information">
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">Menu</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/dcss">Dungeon Crawl</a>
+			<a href="/dcss/online"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online</a>
+			<a href="/dcss/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download DCSS</a>
+			<a href="http://crawl.chaosforge.org/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; DCSS Wiki</a>
+		</div>
+	</div>
+
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Donate!</button>
+		<div class="dropdown-content" style="right:0;">
+			Please donate for the cost of the CCO.CrawlCosplay.Org webtiles server at:<br>
+			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">Patreon</b></a>
+		</div>
+	</div>    <div class="toplinks"><br></div>
+    <div class="page_content">
+        <div class="heading">
+            <h1><img src="/img/dcss_logo.png"></h1>
+	          <div class="fineprint"><center>Come chat with us in our new <a href="https://discord.gg/AasvjYZSMe" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
+        </div>
+	<br>
+        <img src="/img/HR-right.png"><br>
