c81e3fb7acbcf0530b1e4debad512ab742e43026
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 20:35:30 -0400

    Update online_servers.html.php

--- content/pages/dungeoncrawl/online_servers.html.php
+++ content/pages/dungeoncrawl/online_servers.html.php
@@ -7,8 +7,8 @@
     <li><a href="https://cbro.berotato.org" target="_blank">CBRO</a>:      Ohio, USA</li>
     <li><a href="https://crawl.dcss.io" target="_blank">CDI</a>:           New York, USA</li>
     <li><a href="https://crawl.xtahua.com" target="_blank">CXC</a>:        Paris, France</li>
-    <li><a href="https://underhound.eu" target="_blank">CUE</a>:           Falkenstein, Germany</li>
+    <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
     <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
-    <li><a href="https://lazy-life.ddo.jp" target="_blank">LLD</a>:        Tokyo, Japan</li>
+    <li><a href="https://lazy-life.ddo.jp:8080" target="_blank">LLD</a>:   Tokyo, Japan</li>
     <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li>
 </ul>
