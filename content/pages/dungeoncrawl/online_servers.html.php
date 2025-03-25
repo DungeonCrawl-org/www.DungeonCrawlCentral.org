e2bb0e5d542bd7739955807f50c74697d724774e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 20:20:05 -0400

    Update online_servers.html.php

--- content/pages/dungeoncrawl/online_servers.html.php
+++ content/pages/dungeoncrawl/online_servers.html.php
@@ -2,13 +2,13 @@
 
 <h2>Select a server manually:</h2>
 
-<ul><li>Toronto, Canada: <a href="http://cco.crawlcosplay.org">CCO</a></li>
-    <li>Arizona, USA: <a href="http://crawl.akrasiac.org">CCO</a></li>
-    <li>Ohio, USA: <a href="http://cbro.berotato.org">CCO</a></li>
-    <li>New York, USA: <a href="http://crawl.dcss.io">CCO</a></li>
-    <li>Paris, France: <a href="http://crawl.xtahua.com">CCO</a></li>
-    <li>Falkenstein, Germany: <a href="http://underhound.eu">CCO</a></li>
-    <li>Gyeonggi, Korea: <a href="http://crawl.nemelex.cards">CCO</a></li>
-    <li>Tokyo, Japan: <a href="http://lazy-life.ddo.jp">CCO</a></li>
-    <li>Sydney, Australia: <a href="http://crawl.project357.org">CCO</a></li>
+<ul><li>Toronto, Canada:      <a href="http://cco.crawlcosplay.org">CCO</a></li>
+    <li>Arizona, USA:         <a href="https://crawl.akrasiac.org">CAO</a></li>
+    <li>Ohio, USA:            <a href="https://cbro.berotato.org">CBRO</a></li>
+    <li>New York, USA:        <a href="https://crawl.dcss.io">CDI</a></li>
+    <li>Paris, France:        <a href="https://crawl.xtahua.com">CXC</a></li>
+    <li>Falkenstein, Germany: <a href="https://underhound.eu">CUE</a></li>
+    <li>Gyeonggi, Korea:      <a href="https://crawl.nemelex.cards">CNC</a></li>
+    <li>Tokyo, Japan:         <a href="https://lazy-life.ddo.jp">LLD</a></li>
+    <li>Sydney, Australia:    <a href="https://crawl.project357.org">CPO</a></li>
 </ul>
