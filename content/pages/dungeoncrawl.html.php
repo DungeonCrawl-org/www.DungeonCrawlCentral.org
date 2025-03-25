6a769b7934c5485b97d9a43f017e492d8915d828
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-24 20:11:49 -0400

    Update dungeoncrawl.html.php

--- content/pages/dungeoncrawl.html.php
+++ content/pages/dungeoncrawl.html.php
@@ -1,8 +1,8 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h2 style="color:green">Your home for free, open source & actively developped Dungeon Crawl forks.</h2>
+<h2>Your home for free, open source & actively developped Dungeon Crawl (DC) forks.</h2>
 <h2 style="color:green"><img src="/img/feat-dg_entrance.png">Adventure through dungeons filled with dangerous monsters in a quest to find the fabulous:</h2>
 <h1>Orb of Zot</h1>
 <center><img src="/img/orb_of_zot.gif"></center>
 
-<h2><center><a href="/dcss/online">Play Online</a> - <a href="dcss/download">Download DCSS</a></center></h2>
+<h2><center><a href="/dungeoncrawl/online_servers">Play Online</a> - <a href="dcss/download">Download DC forks</a></center></h2>
