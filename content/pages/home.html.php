439ffa654c28fe9e8c50ec99c25f4a0fe2a0c249
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-26 15:30:56 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -10,7 +10,7 @@
 
 	echo '<h3><a href="/dungeoncrawl">Dungeon Crawl</a> (DC) ' . _("is the game with many forks to choose from.") . "</h3>";
 	echo '<ul><li><a href="/dungeoncrawl/online_servers">' . _("Play online on a Webtiles server") . "</a>.</li>";
-	echo     '<li><a href="/dungeoncrawl/download">' . _("Download an offline version") . "</a>.</li></ul>";
+	echo     '<li><a href="/dungeoncrawl/download_forks">' . _("Download an offline version of one of the DC forks") . "</a>.</li></ul>";
 
 	echo "<br><p>" . _("Please select your Crawl Cosplay pleasure among the following options") . ":";
 	echo '<h3><a href="/cca">' . _("Crawl Cosplay Academy") . "</a> (CCA)</h3>";
