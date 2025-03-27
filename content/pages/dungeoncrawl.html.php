92b532a66591c7a6c064367bdb77a82c05c03117
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-27 00:18:12 -0400

    Update dungeoncrawl.html.php

--- content/pages/dungeoncrawl.html.php
+++ content/pages/dungeoncrawl.html.php
@@ -6,3 +6,10 @@
 <center><img src="/img/orb_of_zot.gif"></center>
 
 <h2><center><a href="/dungeoncrawl/online_servers">Play Online</a> - <a href="dcss/download">Download DC forks</a></center></h2>
+<p><img src="/img/HR-right.png"></p>
+<h2>Other links</h2>
+<ul><li><a href="http://crawl.chaosforge.org/" target="_blank">Crawl Wiki</a></li>
+    <li><a href="/">Dungeon Crawl Cosplay</a></li>
+    <li><a href="">Dungeon Crawl Cosplay 2.0 community discord server</a></li>
+    <li><a href="https://tavern.dcss.io/" target="_blank">Tavern 2.0</a></li>
+</ul>
