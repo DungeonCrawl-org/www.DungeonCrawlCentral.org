699660a9345febe18aaeb04015600a603a1c70c4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-27 20:18:42 -0400

    Update forks.html.php

--- content/pages/dungeoncrawl/forks.html.php
+++ content/pages/dungeoncrawl/forks.html.php
@@ -1,15 +1,42 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h1 style="color:orange">Dungeon Crawl actively developped forks</h1>
+<h2 style="color:orange">Dungeon Crawl actively developped forks</h2>
 
-<h2>Click on the link to get more info on that fork or click arrow to see the list of WebTiles servers with that fork installed</h2>
+<h4>Click on the link to get more info on that fork or click arrow to see the list of WebTiles servers with that fork installed</h4>
 
+<details style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="/dungeoncrawl/bcadren" target="_blank">BCadren</a></summary>
+            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
+                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
+                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
+</details></p>
 <details style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/dungeoncrawl/bcrawl" target="_blank">BCrawl</a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
                 <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
+                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
+</details></p>
+<details style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="/dungeoncrawl/stoatsoup" target="_blank">StoatSoup</a></summary>
+            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
+                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
-            </ul>
+                <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
+</details></p>
+<details style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="/dungeoncrawl/stonesoup" target="_blank">Stone Soup</a> (DCSS)</summary>
+            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:         Toronto, Canada</li>
+                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:             Seattle, USA</li>
+                <li><a href="https://crawl.akrasiac.org:8443" target="_blank">CAO</a>:     Arizona, USA</li>
+                <li><a href="https://cbro.berotato.org" target="_blank">CBRO</a>:          Ohio, USA</li>
+                <li><a href="https://crawl.dcss.io" target="_blank">CDI</a>:               New York, USA</li>
+                <li><a href="https://crawl.xtahua.com" target="_blank">CXC</a>:            Paris, France</li>
+                <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:          Falkenstein, Germany</li>
+                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:         Gyeonggi, Korea</li>
+                <li><a href="http://lazy-life.ddo.jp:8080/#lobby" target="_blank">LLD</a>: Tokyo, Japan</li></ul>
 </details></p>
