37ac5a21e2a5c6bbf21afdb55033f13b25eb2d20
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-27 20:03:57 -0400

    Create forks.html.php

---
+++ content/pages/dungeoncrawl/forks.html.php
@@ -0,0 +1,15 @@
+<?php $this->layout = 'dungeoncrawl'; ?>
+
+<h1 style="color:orange">Dungeon Crawl actively developped forks</h1>
+
+<h2>Click on the link to get more info on that fork or click arrow to see the list of WebTiles servers with that fork installed</h2>
+
+<details style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="/dungeoncrawl/bcrawl" target="_blank">BCrawl</a></summary>
+            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
+                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
+                <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
+                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
+            </ul>
+</details></p>
