30eaa009955ffed338745f90ab157db98520a411
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-31 09:11:59 -0400

    Update neck.html.php

--- content/layouts/dungeoncrawl/neck.html.php
+++ content/layouts/dungeoncrawl/neck.html.php
@@ -2,12 +2,13 @@
 	<div class="dropdown" style="float:left;">
 		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content" style="left:0;">
-			<a href="/dungeoncrawl">Dungeon Crawl</a>
-			<a href="/dungeoncrawl/online_servers"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online</a>
-			<a href="/dungeoncrawl/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download DCSS</a>
+			<a href="/dungeoncrawl">Dungeon Crawl (DC)</a>
+			<a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server</a>
+			<a href="/dungeoncrawl/online_servers"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online on a Webtiles server</a>
+			<a href="/dungeoncrawl/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a DC fork for offline play</a>
 			<a href="/dungeoncrawl/forks"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; DC forks details</a>
-			<a href="http://crawl.chaosforge.org/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; DCSS Wiki</a>
-			<a href="/">Crawl Cosplay website</a>
+			<a href="http://crawl.chaosforge.org/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl Wiki</a>
+			<a href="https://www.crawlcosplay.org/">Crawl Cosplay website</a>
 			<a href="https://discord.gg/pW7nqC8Wu3" target="_blank">Crawl Cosplay 2.0 community discord server</a>
 		</div>
 	</div>
@@ -22,7 +23,7 @@
     <div class="page_content">
         <div class="heading">
             <h1><img src="/img/dc_logo.png"></h1>
-	          <div class="fineprint"><center>Come chat with us in our new <a href="https://discord.gg/AasvjYZSMe" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
+	          <div class="fineprint"><center>Come chat with us in our new <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
         </div>
 	<br>
         <img src="/img/HR-right.png"><br>
