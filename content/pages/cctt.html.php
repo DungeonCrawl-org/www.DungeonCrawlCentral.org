c7843fbd5dd049667d6d82228331ca92a064b8c3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-01 01:10:40 -0400

    Update cctt.html.php

--- content/pages/cctt.html.php
+++ content/pages/cctt.html.php
@@ -111,7 +111,6 @@ if ($active) :
 <?php else : // no active challenge
 	// echo $this->part('pause');
 	echo "<h2>There is currently no active Crawl Cosplay Trunk Tournament (CCTT)</h2>";
-	echo "<br>";
 	echo 'For previous tournament results, see:';
 	echo '<ul><li><a href="/cctt/tresults.html?set=32">CCTT#1</a> June 2024</li>';
 	echo     '<li><a href="/cctt/tresults.html?set=33">CCTT#2</a> March 2025</li>';
