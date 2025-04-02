1250b63544d67cb73770c73a991a2b6e02669f86
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-02 16:07:23 -0400

    Update forks.html.php

--- content/pages/dungeoncrawl/forks.html.php
+++ content/pages/dungeoncrawl/forks.html.php
@@ -4,14 +4,14 @@
 
 <h4>Click on the link to get more info on that fork or click arrow to see the list of WebTiles servers with that fork installed</h4>
 <br>
-<details style="display:inline-block;">
+<details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/dungeoncrawl/forks/bcadren">BCadren</a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
                 <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
 </details></p>
-<details style="display:inline-block;">
+<details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/dungeoncrawl/forks/bcrawl">BCrawl</a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
@@ -19,7 +19,7 @@
                 <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
 </details></p>
-<details style="display:inline-block;">
+<details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/dungeoncrawl/forks/stoatsoup">StoatSoup</a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
@@ -27,7 +27,7 @@
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
                 <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
 </details></p>
-<details style="display:inline-block;">
+<details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/dungeoncrawl/forks/stonesoup">Stone Soup</a> (DCSS)</summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:         Toronto, Canada</li>
