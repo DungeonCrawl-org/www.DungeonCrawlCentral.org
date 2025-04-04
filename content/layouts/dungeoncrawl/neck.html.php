4768974051a5e87cad59de27ef611d462faf09ae
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-04 15:58:36 -0400

    Update neck.html.php

--- content/layouts/dungeoncrawl/neck.html.php
+++ content/layouts/dungeoncrawl/neck.html.php
@@ -2,10 +2,10 @@
 	<div class="dropdown" style="float:left;">
 		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content" style="left:0;">
-			<a href="/dungeoncrawl">Dungeon Crawl (DC) website</a>
+			<a href="/dungeoncrawl">Dungeon Crawl Central website</a>
 			<a href="https://discord.gg/gMnE5JFcB7" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl community discord server</a>
 			<a href="/dungeoncrawl/online_servers"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online on a Webtiles server</a>
-			<a href="/dungeoncrawl/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a DC fork for offline play</a>
+			<a href="/dungeoncrawl/download"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a Dungeon Crawl fork for offline play</a>
 			<a href="/dungeoncrawl/forks"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl forks details</a>
 			<a href="http://crawl.chaosforge.org/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl Wiki</a>
 			<a href="https://tavern.dcss.io/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl Tavern 2.0 dicussion forum</a>
@@ -23,7 +23,7 @@
 	</div>    <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
-            <h1><img src="/img/feat-dg_entrance.png"> <a href="https://www.DungeonCrawlForks.org">www.DungeonCrawlForks.org</a> <img src="/img/feat-dg_entrance.png"></h1>
+            <h1><img src="/img/feat-dg_entrance.png"> <a href="https://www.DungeonCrawlCentral.org">www.DungeonCrawlCentral.org</a> <img src="/img/feat-dg_entrance.png"></h1>
 	          <div class="fineprint"><center>Come chat with us in our new <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
         </div>
 	<br>
