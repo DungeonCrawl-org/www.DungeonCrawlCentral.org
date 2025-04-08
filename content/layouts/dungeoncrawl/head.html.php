028f38950ebf2333c26e686afd03dcaaa639675a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-08 16:16:31 -0400

    Update head.html.php

--- content/layouts/dungeoncrawl/head.html.php
+++ content/layouts/dungeoncrawl/head.html.php
@@ -10,6 +10,30 @@
 
 <style>
 
+#dlTable {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#dlTable td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#dlTable tr:nth-child(even){background-color: #f2f2f2;}
+
+#dltable tr:hover {background-color: #ddd;}
+
+#dlTable th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+
+
 div.score-sidebar-cca {
     float: right;
     clear: both;
