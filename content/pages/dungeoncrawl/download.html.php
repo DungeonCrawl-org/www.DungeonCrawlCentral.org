4421c39be1caa0471f19e8cb7ea3b57c72084a08
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-07 23:11:14 -0400

    Update download.html.php

--- content/pages/dungeoncrawl/download.html.php
+++ content/pages/dungeoncrawl/download.html.php
@@ -1,10 +1,11 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
 <h1>Download Stone Soup fork</h1>
+<br>
 <table style="text-align: center;">
-    <thead><tr><th><h5 class="text-center">Latest Stable Version: <a href="https://github.com/crawl/crawl/releases/tag/0.32.1"><strong>0.32.1</strong></a></h5></th>
-               <th><h3 class="text-center">Graphical Tiles</h3></th>
-               <th><h3 class="text-center">Console</h3></th>
+    <thead><tr><th><h2 class="text-center">Latest Stable Version: <a href="https://github.com/crawl/crawl/releases/tag/0.32.1"><strong>0.32.1</strong></a></h2></th>
+               <th><h2 class="text-center">Graphical Tiles</h2></th>
+               <th><h2 class="text-center">Console</h2></th>
            </tr></thead>
     <tbody><tr><th scope="row">Windows Installer</th>
                <td colspan=2><a href="https://github.com/crawl/crawl/releases/download/0.32.1/dcss-0.32.1-win32-installer.exe">Download Tiles+Console</a></td>
