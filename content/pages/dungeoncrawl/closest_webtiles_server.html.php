d71694b56d2f8d7bc8aed3d0331cda99b2878496
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-08 15:55:48 -0400

    Update closest_webtiles_server.html.php

--- content/pages/dungeoncrawl/closest_webtiles_server.html.php
+++ content/pages/dungeoncrawl/closest_webtiles_server.html.php
@@ -1,7 +1,5 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="/dungeoncrawl/server.js"></script>
 
 <h2 style="color:orange">Closest Dungeon Crawl WebTiles server</h2>
