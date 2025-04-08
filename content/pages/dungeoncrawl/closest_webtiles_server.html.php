8fe2b58b23a7260f373c3502e5e9f0fbed231b07
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-08 15:36:25 -0400

    Update closest_webtiles_server.html.php

--- content/pages/dungeoncrawl/closest_webtiles_server.html.php
+++ content/pages/dungeoncrawl/closest_webtiles_server.html.php
@@ -1,5 +1,11 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h1 style="color:orange">Closest WebTiles server</h1>
+<h1 style="color:orange">Closest Dungeon Crawl WebTiles server</h1>
 <p>Select the option to allow this page to determine your location</p>
 
+<div class="container">
+  <div id="play"><h2 id="play-status" class="text-center">Finding closest WebTiles server...</h2>
+                 <p class="text-center" id="play-list-message">Or select a server manually:</p>
+                 <ul class="list-unstyled" id="play-list" style="width: 100%; text-align: center;"><li>Loading...</li></ul>
+  </div>
+</div>
