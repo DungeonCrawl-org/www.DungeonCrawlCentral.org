ad58878b7ddf4ecdfa1b9ffaeddcff9c1c45038d
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-08 15:54:10 -0400

    Update closest_webtiles_server.html.php

--- content/pages/dungeoncrawl/closest_webtiles_server.html.php
+++ content/pages/dungeoncrawl/closest_webtiles_server.html.php
@@ -4,11 +4,11 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="/dungeoncrawl/server.js"></script>
 
-<h4 style="color:orange">Closest Dungeon Crawl WebTiles server</h4>
+<h2 style="color:orange">Closest Dungeon Crawl WebTiles server</h2>
 <p>Select the option to allow this page to determine your location</p>
 
 <div class="container">
-  <div id="play"><h2 id="play-status" class="text-center">Finding closest WebTiles server...</h2>
+  <div id="play"><h4 id="play-status" class="text-center">Finding closest WebTiles server...</h4>
                  <p class="text-center" id="play-list-message">Or select a server manually:</p>
                  <ul class="list-unstyled" id="play-list" style="width: 100%; text-align: center;"><li>Loading...</li></ul>
   </div>
