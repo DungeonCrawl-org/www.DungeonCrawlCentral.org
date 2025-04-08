76c6e3db083b1d3d3829927cf5e8af495dc3d3d5
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-07 21:19:15 -0400

    Update online_servers.html.php

--- content/pages/dungeoncrawl/online_servers.html.php
+++ content/pages/dungeoncrawl/online_servers.html.php
@@ -11,20 +11,20 @@
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="http://cco.crawlcosplay.org" target="_blank"><b>CCO</b></a>:     Toronto, Canada</summary>
-            <ul><li>Stone Soup v0.11 to v.32 and unstable Trunk</li>
-                <li>bcrawl</li>
+            <ul><li>bcrawl</li>
                 <li>bcadren</li>
                 <li>stoatsoup</li>
+                <li>Stone Soup v0.11 to v0.32 and unstable Trunk</li>
             </ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.yiuf.org/" target="_blank"><b>CYO</b></a>:     Seattle, USA</summary>
-            <ul><li>Stone Soup v0.11 to v.32 and unstable Trunk</li>
-                <li>bcrawl</li>
+            <ul><li>bcrawl</li>
                 <li>bcadren</li>
                 <li>stoatsoup</li>
+                <li>Stone Soup v0.11 to v0.32 and unstable Trunk</li>
             </ul>
 </details></td>
 </tr>
@@ -32,44 +32,44 @@
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.akrasiac.org:8443" target="_blank"><b>CAO</b></a>: Arizona, USA</summary>
-            <ul><li>Stone Soup v0.11 to v.32 and unstable Trunk</li></ul>
+            <ul><li>Stone Soup v0.11 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                  <a href="https://cbro.berotato.org" target="_blank"><b>CBRO</b></a>:      Ohio, USA</summary>
-            <ul><li>Stone Soup v0.24 to v.32 and unstable Trunk</li></ul>
+            <ul><li>Stone Soup v0.24 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 </tr>
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.dcss.io" target="_blank"><b>CDI</b></a>:           New York, USA</summary>
-            <ul><li>Stone Soup v0.30 to v.32 and unstable Trunk</li></ul>
+            <ul><li>Stone Soup v0.30 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.xtahua.com" target="_blank"><b>CXC</b></a>:        Paris, France</summary>
-            <ul><li>Stone Soup v0.25 to v.32 and unstable Trunk</li></ul>
+            <ul><li>Stone Soup v0.25 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 </tr>
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://underhound.eu:8080" target="_blank"><b>CUE</b></a>:      Falkenstein, Germany</summary>
-            <ul><li>Stone Soup v0.10 to v.32 and unstable Trunk</li>
-                <li>bcrawl</li>
+            <ul><li>bcrawl</li>
+                <li>Stone Soup v0.10 to v0.32 and unstable Trunk</li>
             </ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.nemelex.cards" target="_blank"><b>CNC</b></a>:     Gyeonggi, Korea</summary>
-            <ul><li>Stone Soup v0.30 to v.32 and unstable Trunk</li>
-                <li>bcrawl</li>
+            <ul><li>bcrawl</li>
                 <li>bcadren</li>
                 <li>stoatsoup</li>
+                <li>Stone Soup v0.30 to v0.32 and unstable Trunk</li>
             </ul>
 </details></td>
 </tr>
@@ -83,8 +83,8 @@
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="https://crawl.project357.org" target="_blank"><b>CPO</b></a>:    Sydney, Australia</summary>
-            <ul><li>Stone Soup v0.25 to v.32 and unstable Trunk</li>
-                <li>stoatsoup</li>
+            <ul><li>stoatsoup</li>
+                <li>Stone Soup v0.25 to v.32 and unstable Trunk</li>
             </ul>
 </details></td>
 </tr>
