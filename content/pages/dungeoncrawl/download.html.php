cd9b7bf82db8236da3cc716f15a57aedb75c80f1
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-10 08:06:30 -0400

    Update download.html.php

--- content/pages/dungeoncrawl/download.html.php
+++ content/pages/dungeoncrawl/download.html.php
@@ -1,5 +1,5 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h4><a href="/dungeoncrawl/download_dcss" target="_blank">Download the BCrawl fork</a></h4>
+<h4>Download the BCrawl fork</h4>
 
-<h4><a href="/dungeoncrawl/download_dcss" target="_blank">Download the Stone Soup (DCSS) fork</a></h4>
+<h4><a href="/dungeoncrawl/download_dcss">Download the Stone Soup (DCSS) fork</a></h4>
