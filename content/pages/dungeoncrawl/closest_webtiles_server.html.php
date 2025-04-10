bde6902be5ef3853360e131ac3c18afe75c64419
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-10 15:22:26 -0400

    Update closest_webtiles_server.html.php

--- content/pages/dungeoncrawl/closest_webtiles_server.html.php
+++ content/pages/dungeoncrawl/closest_webtiles_server.html.php
@@ -8,6 +8,6 @@
 <div class="container">
   <div id="play"><h4 id="play-status" class="text-center">Finding closest WebTiles server...</h4>
                  <p class="text-center" id="play-list-message">Or select a server manually:</p>
-                 <ul class="list-unstyled" id="play-list" style="width: 100%; text-align: center;"><li>Loading...</li></ul>
+                 <ul class="list-unstyled" id="play-list" style="width: 100%;"><li>Loading...</li></ul>
   </div>
 </div>
