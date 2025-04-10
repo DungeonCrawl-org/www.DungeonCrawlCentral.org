4716455c63f686f6d0a94676b11819b756ba0904
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-10 13:39:58 -0400

    Update download.html.php

--- content/pages/dungeoncrawl/download.html.php
+++ content/pages/dungeoncrawl/download.html.php
@@ -1,7 +1,6 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h4>Download the BCrawl fork</h4>
-
+<h4><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download the BCrawl fork</a></h4>
+<h4><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download the BCadren fork</a></h4>
 <h4><a href="https://github.com/damerell/crawl/releases" target="_blank">Download the Stoat Soup fork</a></h4>
-
 <h4><a href="/dungeoncrawl/download_dcss">Download the Stone Soup (DCSS) fork</a></h4>
