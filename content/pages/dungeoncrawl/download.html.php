56c49390cf9798833e131b7db02a2da13651cfe3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-10 07:58:19 -0400

    Update download.html.php

--- content/pages/dungeoncrawl/download.html.php
+++ content/pages/dungeoncrawl/download.html.php
@@ -1,7 +1,7 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
-<h1>Download Stone Soup fork</h1>
-<h2><center>Latest Stable Version: <a href="https://github.com/crawl/crawl/releases/tag/0.32.1"><strong>0.32.1</strong></a></center></h2>
+<h1>Download the Stone Soup (DCSS) fork</h1>
+<h2><center>Latest Stable Version: <a href="https://github.com/crawl/crawl/releases/tag/0.32.1" target="_blank"><strong>0.32.1</strong></a></center></h2>
 <br>
 <table id="dlTable"><center>
     <thead><tr><td></td>
@@ -26,7 +26,7 @@
                <td><center><a href="https://f-droid.org/en/packages/org.develz.crawl/"><img alt="Get it on F-Droid" src="https://fdroid.gitlab.io/artwork/badge/get-it-on.png" width="210px" height="80px"></a></center></td></tr>
            <tr><th scope="row">Source</th>
                <td colspan="2"><center><a href="https://github.com/crawl/crawl/releases/download/0.32.1/stone_soup-0.32.1.zip">Full source</a></center>
-                               <center><a href="https://github.com/crawl/crawl/releases/tag/0.32.1">Other formats</a></center>
+                               <center><a href="https://github.com/crawl/crawl/releases/tag/0.32.1" target="_blank">Other formats</a></center>
                                <center><a href="#source">See Git Instructions Below</a></td></center></tr>
             <tr><th scope="row">Past Releases</th>
                 <td colspan="2"><center><a href="https://crawl.develz.org/release/?C=N;O=D" target="_blank">Releases Folder</a></center></td></tr>
