74b9f79152655e07c59186c9d679a57943284dde
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-11 08:45:57 -0400

    Update online_servers.html.php

--- content/pages/dungeoncrawl/online_servers.html.php
+++ content/pages/dungeoncrawl/online_servers.html.php
@@ -30,39 +30,39 @@
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://crawl.akrasiac.org:8443" target="_blank"><b>CAO</b></a>: Arizona, USA - Webtiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                <a href="https://crawl.akrasiac.org:8443" target="_blank"><b>CAO</b></a>: Arizona, USA - Webtiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>Stone Soup v0.11 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                 <a href="https://cbro.berotato.org" target="_blank"><b>CBRO</b></a>:      Ohio, USA - WebTiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                 <a href="https://cbro.berotato.org" target="_blank"><b>CBRO</b></a>:      Ohio, USA - WebTiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>Stone Soup v0.24 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 </tr>
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://crawl.dcss.io" target="_blank"><b>CDI</b></a>:           New York, USA - WebTiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                <a href="https://crawl.dcss.io" target="_blank"><b>CDI</b></a>:           New York, USA - WebTiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>Stone Soup v0.30 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://crawl.develz.org" target="_blank"><b>CDO</b></a>:        Nuremberg, Germany - <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console ONLY</a></summary>
+                <a href="https://crawl.develz.org" target="_blank"><b>CDO</b></a>:        Nuremberg, Germany - <a href="console_servers" target="_blank">Console ONLY</a></summary>
             <ul><li>Stone Soup v0.2x to v0.32 and unstable Trunk</li></ul>
 </details></td>
 </tr>
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://crawl.xtahua.com" target="_blank"><b>CXC</b></a>:        Paris, France - WebTiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                <a href="https://crawl.xtahua.com" target="_blank"><b>CXC</b></a>:        Paris, France - WebTiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>Stone Soup v0.25 to v0.32 and unstable Trunk</li></ul>
 </details></td>
 
 <td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://underhound.eu:8080" target="_blank"><b>CUE</b></a>:      Falkenstein, Germany - Webtiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                <a href="https://underhound.eu:8080" target="_blank"><b>CUE</b></a>:      Falkenstein, Germany - Webtiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>bcrawl</li>
                 <li>Stone Soup v0.10 to v0.32 and unstable Trunk</li>
             </ul>
@@ -71,7 +71,7 @@
 
 <tr><td><details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="https://crawl.nemelex.cards" target="_blank"><b>CNC</b></a>:     Gyeonggi, Korea - WebTiles & <a href="https://crawl.develz.org/wordpress/howto" target="_blank">Console</a></summary>
+                <a href="https://crawl.nemelex.cards" target="_blank"><b>CNC</b></a>:     Gyeonggi, Korea - WebTiles & <a href="console_servers" target="_blank">Console</a></summary>
             <ul><li>bcrawl</li>
                 <li>bcadren</li>
                 <li>stoatsoup</li>
