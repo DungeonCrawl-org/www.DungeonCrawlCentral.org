79fb81c297baa6c3a691b9993a1a9581c23f7d74
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-10 21:12:33 -0400

    Create stone_soup.html.php

---
+++ content/pages/dungeoncrawl/forks/stone_soup.html.php
@@ -0,0 +1,4 @@
+<?php $this->layout = 'dungeoncrawl'; ?>
+
+<a href="http://crawl.chaosforge.org/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Wiki</a>
+<a href="https://tavern.dcss.io/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Tavern 2.0 dicussion forum</a>
