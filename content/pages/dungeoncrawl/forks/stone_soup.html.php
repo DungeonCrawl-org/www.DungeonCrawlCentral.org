b535bab8fc0d82c87cd07e85f385ca3e0fe7a453
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-11 08:17:04 -0400

    Update stone_soup.html.php

--- content/pages/dungeoncrawl/forks/stone_soup.html.php
+++ content/pages/dungeoncrawl/forks/stone_soup.html.php
@@ -1,4 +1,8 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
+<p>Server statistics, high-scores and milestones are gathered in a single place, however separately categorized by 
+    <a href="http://crawl.akrasiac.org/scoring-stable/overview.html">stable/released only</a> or 
+    <a href="http://crawl.akrasiac.org/scoring/overview.html">all stable/released &amp; alpha/development</a> games.</p>
+
 <a href="http://crawl.chaosforge.org/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Wiki</a>
 <a href="https://tavern.dcss.io/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Tavern 2.0 dicussion forum</a>
