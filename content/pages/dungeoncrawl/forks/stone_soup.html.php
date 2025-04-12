b479009aef3bce2f041cd0845ba0114dea080105
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-12 08:39:14 -0400

    Update stone_soup.html.php

--- content/pages/dungeoncrawl/forks/stone_soup.html.php
+++ content/pages/dungeoncrawl/forks/stone_soup.html.php
@@ -1,8 +1,12 @@
 <?php $this->layout = 'dungeoncrawl'; ?>
 
+<h1 style="color:orange">Stone Soup</h1>
+
+<h2>Dungeon Crawl: Stone Soup (DCSS): a Dungeon Crawl fork based on <a href="http://dungeoncrawl.org/" target="_blank">Linley's Dungeon Crawl</a>: the orginal game!</h2>
+
 <p>Server statistics, high-scores and milestones are gathered in a single place, however separately categorized by 
     <a href="http://crawl.akrasiac.org/scoring-stable/overview.html">stable/released only</a> or 
     <a href="http://crawl.akrasiac.org/scoring/overview.html">all stable/released &amp; alpha/development</a> games.</p>
 
-<a href="http://crawl.chaosforge.org/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Wiki</a>
-<a href="https://tavern.dcss.io/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Tavern 2.0 dicussion forum</a>
+<p><a href="http://crawl.chaosforge.org/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Wiki</a></p>
+<p><a href="https://tavern.dcss.io/" target="_blank">Dungeon Crawl Stone Soup (DCSS) Tavern 2.0 dicussion forum</a></p>
