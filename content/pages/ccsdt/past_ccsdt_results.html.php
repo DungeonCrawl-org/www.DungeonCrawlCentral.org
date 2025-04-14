143752d89a8136f7be9964f4854236dc358d0045
RoGGa <robert_gagne69@yahoo.com>
2025-04-14 10:58:28 -0400

    restructure website

--- content/pages/ccsdt/past_ccsdt_results.html.php
+++
@@ -1,5 +0,0 @@
-<?php $this->layout = 'ccsdt'; ?>
-
-<h2>Past Crawl Cosplay Suddent Death Tournament (CCSDT) results</h2>
-
-<ul><li><a href="/ccsdt/0.32/standings.html">CCSDT#1 results</a> for v0.32.</li></ul>
